39e04ba107b14277c249d962f64c82299f3b65c2
pesto <giulio.pastore2410@gmail.com>
2023-05-12 17:12:47 +0200

    First exercise

--- src/main/java/com/example/springboot/controller/HelloController.java
+++ src/main/java/com/example/springboot/controller/HelloController.java
@@ -1,11 +1,14 @@
 package com.example.springboot.controller;
 
+import com.mysql.cj.x.protobuf.Mysqlx;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Random;
+
+import static javax.security.auth.callback.ConfirmationCallback.OK;
 
 @RestController
 public class HelloController {
@@ -22,18 +25,18 @@ public class HelloController {
 @RestController
 class HelloController2 {
 	@GetMapping("/info")
-	public ResponseEntity<String> getInfo() {
+	public ResponseEntity getInfo() {
 		return new ResponseEntity(HttpStatusCode.valueOf(200));
 	}
 }
 @RestController
 class HelloController3 {
 	@GetMapping("/random")
-	public ResponseEntity<String> random() {
+	public ResponseEntity random() {
 		boolean random = new Random().nextBoolean();
 		if(random) {
-			return ResponseEntity.ok("It's true");
+			return new ResponseEntity("It's true", HttpStatusCode.valueOf(200));
 		}
-		return ResponseEntity.badRequest().body("It's false!");
+		return new ResponseEntity("It's false", HttpStatusCode.valueOf(400));
 	}
 }
