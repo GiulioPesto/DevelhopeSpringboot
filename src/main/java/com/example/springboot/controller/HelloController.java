1a6f2f96163954d5c0d8e45979d95942d17e3155
pesto <giulio.pastore2410@gmail.com>
2023-05-12 16:07:25 +0200

    First exercise in HelloController class

--- src/main/java/com/example/springboot/controller/HelloController.java
+++ src/main/java/com/example/springboot/controller/HelloController.java
@@ -1,13 +1,40 @@
 package com.example.springboot.controller;
 
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 @RestController
 public class HelloController {
-	@GetMapping(value = "/good-morning")
-	public ResponseEntity<String> index() {
-		return ResponseEntity.ok("Good morning!");
+	@GetMapping("/hello")
+	public String index() {
+		return "Hello World!";
+	}
+	@GetMapping("/greeting")
+	public ResponseEntity<String> greeting() {
+		return new ResponseEntity("Good Afternoon!", HttpStatusCode.valueOf(200));
+	}
+}
+
+@RestController
+class HelloController2 {
+	@GetMapping("/info")
+	public ResponseEntity<String> getInfo() {
+		return ResponseEntity.ok("200 OK!");
+	}
+
+	@RestController
+	class HelloController3 {
+		@GetMapping("/random")
+		public ResponseEntity<String> random() {
+			boolean random = new Random().nextBoolean();
+			if(random) {
+				return ResponseEntity.ok("It's true");
+			}
+			return ResponseEntity.badRequest().body("It's false!");
+		}
 	}
 }
